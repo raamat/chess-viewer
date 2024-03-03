1313c2c28ed4e8a91a1dd6120e3c3eeb148b42cc
Raamat Volgograd <Gadjukino777@yandex.ru>
2024-03-03 10:19:12 +0300

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,23 @@
+<template>
+   <div class="container">
+    <div class="main"><Chessboard /></div>
+    <div class="sidebar">
+      <button class="btn">Жми</button>
+    </div>
+    
+   </div>
+</template>
+
+<script setup lang="ts">
+import { Chessboard } from "cw-chessboard";
+</script>
+
+<style scoped>
+  .main {
+    width: 500px;
+    height: 500px;
+    background-color: black;
+  }
+
+
+</style>
