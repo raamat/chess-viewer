d696c59ffd0bc7a3b5930ff2f22273cdeca5d799
Raamat <raamat@bk.ru>
2024-03-07 11:26:28 +0300

    add block-fen-pgn

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,24 @@
 <template>
-   <div class="container">
-    <div class="main"><Chessboard /></div>
-    <div class="sidebar">
-      <button class="btn">Жми</button>
+  <div class="flex flex-row pt-6">
+    <div class="basis-1/4"></div>
+    <div class="basis-2/4">
+      <Chessboard />
+      <div class="block-fen-pgn mt-6">
+        <div class="flex">
+        <label class="font-medium w-12">FEN</label>
+        <input type="text" class="border-inherit border-2 w-full">
+      </div>
+      <div class="flex mt-6 items-center">
+        <label class="font-medium w-12">PGN</label>
+        <textarea class="border-inherit border-2 w-full"></textarea>
+      </div>
+      </div>
+      
     </div>
-    
-   </div>
+    <div class="basis-1/4"></div>
+  </div>
 </template>
 
 <script setup lang="ts">
 import { Chessboard } from "cw-chessboard";
 </script>
-
-<style scoped>
-  .main {
-    width: 500px;
-    height: 500px;
-    background-color: black;
-  }
-
-
-</style>
